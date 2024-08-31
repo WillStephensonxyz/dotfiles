a2d6500b0a78115310c0f1b647b7fa2764546947
WillStephensonxyz <willstephenson66@gmail.com>
2024-08-31 14:06:45 +1000

    updated set monitors for docking station

--- set-monitors.sh
+++ set-monitors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #
 # Set secondary monitor 
-xrandr --output "DVI-I-2-2" --mode "1920x1080" --right-of eDP-1
+xrandr --output "DVI-I-1-1" --mode "1920x1080" 
 
 # Set third monitor to the right of secondary 
-xrandr --output "DVI-I-1-1" --mode "1920x1080" --right-of DVI-I-2-2
+xrandr --output "DVI-D-1" --mode "1920x1080" --right-of DVI-I-1-1
